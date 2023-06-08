9351fb04c68f92c4271fab66a1efd4f219318b13
Douglas Loomis <loomisdk2@gmail.com>
2023-06-08 10:50:57 -0500

    Caesar: corrected line 67:48

--- src/caesar.js
+++ src/caesar.js
@@ -64,7 +64,7 @@ if(encode) {
     if(shiftedCharCode > charCodeZ) {
       return String.fromCharCode(charCodeA + ((shiftedCharCode - charCodeZ) - 1));
     } else if (shiftedCharCode < charCodeA) {
-      return String.fromCharCode(charCodeZ - ((charCode - shiftedCharCode) - 1));
+      return String.fromCharCode(charCodeZ - ((charCodeA - shiftedCharCode) - 1));
     } else {
       return String.fromCharCode(shiftedCharCode);
     }
